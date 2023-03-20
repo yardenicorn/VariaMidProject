f8f8ffe20cce2d88476358d0c13820c03175865e
Yarden Admon <yardenadmon@gmail.com>
2023-03-20 09:23:14 +0200

    Update PlayerMovement.cs
    jump & double jump is working

--- Assets/Warped Caves/Scripts/PlayerMovement.cs
+++ Assets/Warped Caves/Scripts/PlayerMovement.cs
@@ -12,17 +12,16 @@ public class PlayerMovement : MonoBehaviour
     public GameObject bullet;
 
     private bool facingRight = true;
-    private bool doublejump = true;
     private float dirX = 0f;
-    public float duckingOffset = 0.52f;
+    private float duckingOffset = 0.52f;
+    private int jumpingCount = 0;
 
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float JumpForce = 14f;
-    [SerializeField] private LayerMask jumpableGround;
+    [SerializeField] private LayerMask Ground;
 
     private enum MovementState { idle, run, jump, shot, duck }
 
-    // Start is called before the first frame update
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -39,27 +38,19 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        // Debug.Log(doublejump);
+        bool isGrounded = Physics2D.Linecast(transform.position,transform.position + Vector3.down, LayerMask.GetMask("Ground"));
         dirX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
 
-        if (IsGrounded() && !Input.GetKeyDown("up"))
+        if (Input.GetKeyDown(KeyCode.UpArrow) && jumpingCount < 1)
         {
-            doublejump = false;
+            rb.velocity = new Vector2(rb.velocity.x, JumpForce);
+            jumpingCount++;
         }
 
-        if (Input.GetKeyDown("up"))
+        if (isGrounded)
         {
-            if (IsGrounded() || doublejump)
-            {
-                rb.velocity = new Vector2(rb.velocity.x, JumpForce);
-                doublejump = !doublejump;
-            }
-        }
-
-        if (Input.GetKeyDown("up") && rb.velocity.y > 0f)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, JumpForce * 1f);
+            jumpingCount = 0;
         }
 
         UpdateAnimationState();
@@ -126,11 +117,5 @@ public class PlayerMovement : MonoBehaviour
         float rotation = FirePoint.rotation.eulerAngles.z;
         rotation = facingRight ? 0f : 180f;
         FirePoint.rotation = Quaternion.Euler(0, 0, rotation);
-    }
-
-    // checking if the player is touching the ground
-    private bool IsGrounded()
-    {
-        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
     }
 }
